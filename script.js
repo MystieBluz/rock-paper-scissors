d7d373e2381458d1395a8959cbf2bbe0283d1e5d
Misty Duhart <mystie.smith@gmail.com>
2022-05-28 11:20:46 -0400

    updated file with player and computer choice functions

--- script.js
+++ script.js
@@ -1,96 +1,32 @@
-const selectionButtons = document.querySelectorAll('[data-selection]')
+// Variables for display and all the game buttons
+const playerDisplay = document.getElementById('playerDisplay');
+const computerDisplay = document.getElementById('computerDisplay');
+const resultDisplay = document.getElementById('result');
+const gameBtns = document.querySelectorAll('button');
 
+// Player and Computer variable
+let playerChoice;
+let computerChoice;
 
+// Function for game buttons
+gameBtns.forEach(gameBtns => gameBtns.addEventListener('click', (e) => {
+  playerChoice = e.target.id
+  playerDisplay.innerHTML = playerChoice;
+  generateComputerChoice()
+}))
 
-// User's Choice Function
-const getUserChoice = userInput => {
-  // userInput = userInput.toLowerCase();
-// User's choices for game
-  if (userInput.onclick === 'rock' || userInput === 'paper' || userInput === 'scissors' || userInput === 'bomb') {
-    return userInput;
-  }
-  else {
-    console.log('Error! You must enter either rock, paper or scissors!');
-  }
-};
-
-// Testing function 
-// console.log(getUserChoice('Rock'));
-
-selectionButtons.forEach(selectionButton => {
-  selectionButton.addEventListener('click', e => {
-    const selectionName = selectionButton.dataset.selection
-    const selection = selection.find(selection => selection.name === selectionName)
-    makeSelection(selection)
-  })
-})
-// Computer's Choice Function
-const getComputerChoice = () => {
-  const randomNumber = (Math.floor(Math.random() * 3));
+const generateComputerChoice = () => {
+  const randomNumber = (Math.floor(Math.random() * 3)); // replace 3 with: gameBtns.length as another option
+  // console.log(randomNumber)
 
   if (randomNumber === 0) {
-    return 'rock';
+    computerChoice = 'Rock';
   }
   else if (randomNumber === 1) {
-    return 'paper';
+    computerChoice = 'Paper';
   }
   else if (randomNumber === 2) {
-    return 'scissors';
+    computerChoice = 'Scissors';
   }
+  computerDisplay.innerHTML = computerChoice
 }
-
-// Testing function
-//  console.log(getComputerChoice());
-
-
-//  Function to determine a winner
-const determineWinner = (userChoice, computerChoice) => {
-  // Cheat code to always win, lol
-  if (userChoice === 'bomb') {
-    return 'You WON!'
-  }
-  // If game is Tied
-  if (userChoice === computerChoice) {
-    return 'It\'s a TIE!'
-  }
-  if (userChoice === 'rock') {
-    if (computerChoice === 'paper'){
-      return 'Computer WINS!'
-    } else {
-      return 'You WON!'
-    }
-  }
-  if (userChoice === 'paper') {
-    if (computerChoice === 'scissors') {
-      return 'Computer WINS!'
-    } else {
-      return 'You WON!'
-    }
-  }
-  if (userChoice === 'scissors') {
-    if (computerChoice === 'rock') {
-      return 'Computer WINS!'
-    } else {
-    return 'You WON!'
-   }
-  } 
-}
-
-// Testing Function
-// console.log(determineWinner('paper', 'scissors'));
-// console.log(determineWinner('paper', 'paper'));
-// console.log(determineWinner('paper', 'rock'));
-
-
-// Function to Start Game and Log results
-const playGame = () => {
-  // Must change *user choices* ONLY each try to get various results
-  const userChoice = getUserChoice('paper');
-  const computerChoice = getComputerChoice();
-  console.log('You choose: ' + userChoice);
-  console.log('Computer choose: ' + computerChoice);
-  // Determine who won
-  console.log(determineWinner(userChoice, computerChoice));
-};
-
-playGame();
