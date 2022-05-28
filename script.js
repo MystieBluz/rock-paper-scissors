d9b56cf1276e7e247b56274bd0a28c7de649def9
Misty Duhart <mystie.smith@gmail.com>
2022-05-28 11:36:22 -0400

    added results function

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-// Variables for display and all the game buttons
+// Variables for display in browser and all the game buttons
 const playerDisplay = document.getElementById('playerDisplay');
 const computerDisplay = document.getElementById('computerDisplay');
 const resultDisplay = document.getElementById('result');
 const gameBtns = document.querySelectorAll('button');
 
-// Player and Computer variable
+// Player, Computer and Results variable
 let playerChoice;
 let computerChoice;
+let gameResults;
 
 // Function for game buttons
 gameBtns.forEach(gameBtns => gameBtns.addEventListener('click', (e) => {
@@ -28,5 +29,39 @@ const generateComputerChoice = () => {
   else if (randomNumber === 2) {
     computerChoice = 'Scissors';
   }
-  computerDisplay.innerHTML = computerChoice
+  computerDisplay.innerHTML = computerChoice;
+}
+
+// Function to determine a winner
+const determineWinner = (playerChoice, computerChoice) => {
+  // Cheat code to always win, lol
+  if (playerChoice === 'bomb') {
+    gameResults = 'You WON!'
+  }
+  // If game is Tied
+  if (playerChoice === computerChoice) {
+    gameResults = 'It\'s a TIE!'
+  }
+  if (playerChoice === 'rock') {
+    if (computerChoice === 'paper'){
+      gameResults = 'Computer WINS!'
+    } else {
+      gameResults = 'You WON!'
+    }
+  }
+  if (playerChoice === 'paper') {
+    if (computerChoice === 'scissors') {
+      gameResults = 'Computer WINS!'
+    } else {
+      gameResults = 'You WON!'
+    }
+  }
+  if (playerChoice === 'scissors') {
+    if (computerChoice === 'rock') {
+      gameResults = 'Computer WINS!'
+    } else {
+      gameResults = 'You WON!'
+   }
+  } 
+  resultDisplay.innerHTML = gameResults;
 }
